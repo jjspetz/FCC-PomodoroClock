ea25b67f5c5439e25844a4cf64715496ddbe0b2b
JJ <jj.spetseris@yahoo.com>
2017-04-02 23:08:13 -0500

    working model

--- JS/script.js
+++ JS/script.js
@@ -4,35 +4,35 @@ var current_session = 25;
 var stop = false;
 
 // hide show because only start or show should ever be visible
-$('#stop').hide();
+$('#stop, #break, #session').hide();
 
 // sets clock to default time
-$('#timer').html(current_session);
+$('#timer').html(displayTime(current_session));
 
 // adjust times for break and session
 $("#break_plus").click(function(){
-  if (current_break < 60) {
+  if (current_break < 59) {
     current_break++;
   }
   $("#break_time").html(current_break);
 })
 
 $("#break_minus").click(function(){
-  if (current_break > 0) {
+  if (current_break > 1) {
     current_break--;
   }
   $("#break_time").html(current_break);
 })
 
 $("#session_plus").click(function(){
-  if (current_session < 60) {
+  if (current_session < 59) {
     current_session++;
   }
   $('#session_time').html(current_session);
 })
 
 $("#session_minus").click(function(){
-  if (current_session > 0) {
+  if (current_session > 1) {
     current_session--;
   }
   $('#session_time').html(current_session);
@@ -48,53 +48,68 @@ $('#start').click(function(){
 
   // declare variables
   var counter;
+  var min;
+  var sec;
   stop = false;
 
   // session time
   function sessionTimer(countdown) {
-    // check to see if clock has reached zero
+    $('#session').show();
+    $('#break, #filler').hide();
+    // check to see if stop has been pressed
     counter = setInterval(function(){
       if (stop) {
         clearInterval(counter);
+        return loop(3);
       }
+      // check to see if clock has reached zero
       if (countdown == 0) {
         clearInterval(counter);
-        $('#timer').html(current_break)
-        return loop(false);
+        $('#timer').html(displayTime(current_break));
+        return loop(2);
       }
-      $('#timer').html(countdown);
+
+      $('#timer').html(displayTime(countdown/60));
       countdown--;
   }, 1000);
 
   }
   // break time
   function breakTimer(countdown) {
-    // check to see if clock has reached zero
+    $('#break').show();
+    $('#session, #filler').hide();
     counter = setInterval(function(){
       if (stop) {
         clearInterval(counter);
+        return loop(3);
       }
+      // check to see if clock has reached zero
       if (countdown == 0) {
         clearInterval(counter);
-        $('#timer').html(current_session)
-        return loop(true);
+        $('#timer').html(displayTime(current_session));
+        return loop(1);
       }
-      $('#timer').html(countdown);
+      $('#timer').html(displayTime(countdown/60));
       countdown--;
     }, 1000);
   }
-  // this loop swicthes in between sessionTimer and breakTimer
-  function loop(active) {
-    if (!stop) {
-      if (active) {
-        sessionTimer(current_session-1);
-      }
-      else {
-        breakTimer(current_break-1);
-      }
+  // this loop swicthes in between sessionTimer, breakTimer, and break
+  function loop(path) {
+    if (path == 1) {
+      displayTime(current_session)
+      sessionTimer(current_session * 60 - 1);
+    }
+    else if (path == 2) {
+      displayTime(current_break)
+      breakTimer(current_break * 60 - 1);
+    }
+    else {
+      $('#timer').html(displayTime(current_session));
     }
   }
-    loop(true);
+
+    // starts the infinite loop on session countdown
+    loop(1);
 })
 
 $('#stop').click(function(){
@@ -104,11 +119,22 @@ $('#stop').click(function(){
   // replaces stop with start
   $('#stop').hide();
   $('#start').show();
+  // hides break and session
+  $('#session, #break').hide();
+  $('#filler').show();
+})
 
-  // figure out how to reset timer
-  // conver seconds to minutes
+function displayTime(time) {
+  //calculates and formats time
+  time = time * 60;
+  min = Math.floor(time / 60);
+  sec = String("0" + (time % 60)).slice(-2);
+
+  return "<div>" + min + ":" + sec + "</div>";
+}
+
+  // add sound
   // pretty up the page
   // add fill to circle
 
-})
 });
