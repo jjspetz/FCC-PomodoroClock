e918697d8922fb98c4b967bc37ec3193f90d7b8e
JJ <jj.spetseris@yahoo.com>
2017-04-09 16:15:31 -0500

    further improvements

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-var current_break = 1;
-var current_session = 1;
+var current_break = 5;
+var current_session = 25;
 var stop = false;
 var start = false;
 
@@ -52,38 +52,55 @@ $('#start').click(function(){
 
   // declare variables
   var counter;
-  var min;
-  var sec;
   stop = false;
   start = true;
 
   // session time
   function sessionTimer(countdown) {
-    // start clock hand moving
+
+    /*******************************
+    * RESET AND STARTS CLOCK HAND FOR EACH NEW SESSION
+    * The invisble-clock dive is created and then distroyed each time in order to
+    * fully stop and restart the animation. Without it the hand jumped around on
+    * start of new session.
+    * The "hand rotates around the clock once per sesssion time.
+    ********************************/
+    $('#inv-clo-con').html('<div id="invisible-clock"><div id="hand"></div><div id="hand2"></div></div>');
     $('#invisible-clock').css("animation",  "countdown "+ (countdown + 1) +"s linear infinite reverse");
+
     // displays "session" inside clock
     $('#session').show();
     $('#break, #start').hide();
-    // check to see if stop has been pressed
+
+    /*********
+    *  sets an interval of one second.
+    *  The code runs every second counting down from the user inputed session time and displays the remaining time
+    *  When time remaining (var countdown) reaches 0 the loop is broken and a breaktime loop begins
+    ***********/
     counter = setInterval(function(){
+
+      // check to see if "stop" button has been pressed if so infinite loop is broken
       if (stop) {
         clearInterval(counter);
         return loop(3);
       }
       // check to see if clock has reached zero
       if (countdown == 0) {
+        $('#inv-clo-con').html(); // deletes all divs inside this container to elemenate any time bleed
         clearInterval(counter);
-        $('#timer').html(displayTime(current_break));
-        return loop(2);
+        $('#timer').html(displayTime(current_break)); // displays the time for breaks
+        return loop(2); // switches infinite loop to display and countdown break time
       }
-      $('#timer').html(displayTime(countdown/60));
+      $('#timer').html(displayTime(countdown/60)); // displays current time (formated in minutes)
       countdown--;
   }, 1000);
 
   }
-  // break time
+  // break time functions similarly to session time
+  // but takes the user inputed length of breaks
   function breakTimer(countdown) {
     // start clock hand moving
+    $('#inv-clo-con').html('<div id="invisible-clock"><div id="hand"></div><div id="hand2"></div></div>');
     $('#invisible-clock').css("animation",  "countdown "+ (countdown + 1) +"s linear infinite reverse");
     // displays "break" in the clock
     $('#break').show();
@@ -95,6 +112,7 @@ $('#start').click(function(){
       }
       // check to see if clock has reached zero
       if (countdown == 0) {
+        $('#inv-clo-con').html()
         clearInterval(counter);
         $('#timer').html(displayTime(current_session));
         return loop(1);
@@ -106,11 +124,11 @@ $('#start').click(function(){
   // this loop swicthes in between sessionTimer, breakTimer, and break
   function loop(path) {
     if (path == 1) {
-      displayTime(current_session)
+      displayTime(current_session);
       sessionTimer(current_session * 60 - 1);
     }
     else if (path == 2) {
-      displayTime(current_break)
+      displayTime(current_break);
       breakTimer(current_break * 60 - 1);
     }
     else {
@@ -122,7 +140,7 @@ $('#start').click(function(){
     loop(1);
 })
 
-// hands what happens when stop is clicked
+// hands what happens when "stop" is clicked
 $('#session, #break').click(function(){
   // kill loop function
   stop = true;
